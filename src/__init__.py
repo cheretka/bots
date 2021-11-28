b71a9aec32c90789a082111a137f4e78dee88374
Kamil Chrustowski <chrustek123456789@gmail.com>
2021-11-28 18:29:17 +0100

    Uploaded a show-case in .ipynb file, that outlines all developed functionalities so far in action.

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
-from .base import Agent, Action, make_env, spawn_random_bots, get_session_id
+from .base import Agent, Action, make_env, spawn_bots, get_session_id
 from .agarnt import AgarntAction, RandomAgent
