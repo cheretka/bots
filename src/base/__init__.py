9b4ff2f1887f93f40a966585f2fe8e091d32e2ff
Kamil Chrustowski <chrustek123456789@gmail.com>
2022-01-09 02:04:20 +0100

    Improved BotsManager to work with multiprocessing.Pool objects instead of concurrent.futures.process.ProcessPoolExecutor

--- src/base/__init__.py
+++ src/base/__init__.py
@@ -1,7 +1,11 @@
 from concurrent.futures import Future
+from multiprocessing import Pool
+from multiprocessing.pool import AsyncResult, Pool as P
 from concurrent.futures.process import ProcessPoolExecutor
 import threading
-from typing import List, Optional, Type
+import traceback
+from typing import List, Optional, Tuple, Type
+import random
 from websockets.legacy.client import connect, WebSocketClientProtocol
 from ._gameproxy import connection_proxy as __connection_proxy, cleanup, Event as _Event
 from .agent import Agent
@@ -24,10 +28,8 @@ class _Bots_Manager:
 	"""Definition of _Bots_Manager to take care of spawned agents.
 	"""
     
-	def __init__(self, executor: ProcessPoolExecutor, futures: List[Future], events: List[_Event]) -> None:
-		self.__executor = executor
-		self.__futures = futures
-		self.__events = events
+	def __init__(self, params: List[Tuple[P, List[AsyncResult], List[_Event]]]) -> None:
+		self.__params = params
   
 	def terminate(self, timeout=10):
 		"""Terminates BotsManager object after given timeout. Sets events to stop managed processes, and collects concurrent.futures.Future objects.
@@ -35,18 +37,21 @@ class _Bots_Manager:
 		Args:
 			timeout (int, optional): time to wait. Defaults to 10.
 		"""
-		for event in self.__events:
-			event.set()
+		for __executor, __futures, __events in self.__params:
+			for event in __events:
+				event.set()
+
+			for future in __futures:
+				try:
+					future.get(timeout=timeout)
+					logger.info("Wait for the AsyncResult at: ", hex(id(future)))
+				except TimeoutError as _time:
+					logger.warn("Unable to kill bot process -> ", _time)
+
 	
-		for future in self.__futures:
-			future.cancel()
-			logger.info("Cancel the future at: ", hex(id(future)))
-   
-		for completed_future in cf.as_completed(self.__futures, timeout=timeout):
-			completed_future.result()
-   
-		logger.info("Manager is about to shutdown...")
-		self.__executor.shutdown()
+	
+			logger.info(f"Pool at: {hex(id(__executor))}, is about to shutdown...")
+			__executor.terminate()
 
 
 def get_session_id(): return __ENV_VARS["session_id"]
@@ -147,7 +152,7 @@ def __run_bot(bot_class: Type[Agent], server: str, session_id: str, int_id: int,
 			nice(20)
 			
 		async def connect_to_url(url): return await connect(url)
-  
+
 		_ = __connection_proxy(connect_to_url)(url)
 		bot = bot_class(**agent_kwds)
 
@@ -168,15 +173,15 @@ def __run_bot(bot_class: Type[Agent], server: str, session_id: str, int_id: int,
    
 		try:
 			while not done and not evt.is_set():
-				sleep(0.1)
+				sleep(random.random())
 				
 				_ = bot.choose_action()
 				logger.info(f"Chosen action {_} goes brr")
 				done = bot.is_done
 			else: logger.warn(f"BOT: {type(bot)} is dead")
 		except Exception as e:
-			logger.warn(e)
-			logger.warn(f"BOT: {type(bot)} is dead")
+			logger.warn(f"BOT: {type(bot)} is dead, exception occurred {e}.")
+			traceback.print_tb(e.__traceback__)
 	wrapper()
 	
 def spawn_bots(server: str, session_id: str, bot_class: Type[Agent], count: int, **agent_kwds):
@@ -189,12 +194,34 @@ def spawn_bots(server: str, session_id: str, bot_class: Type[Agent], count: int,
 		count (int): count of bots to spawn
 		agent_kwds: agent's constructor parameters
 	"""
+	from sys import platform
+	_MAX_COUNT_OF_WIN32_WAIT_FOR_MULTIPLE_OBJECTS = 63 - 2 #2 workers needed as a overhead for ProcessPoolExecutor
+ 
+	def __make(count_of_workers: int, curr:int =0):
+		executor = Pool(processes=count_of_workers)
+		
+		_events = [_Event() for _ in range(count_of_workers)]
+		_futures = {num: executor.apply_async(__run_bot, (bot_class, server, session_id, num, _events[num]), agent_kwds, callback=lambda _: logger.info(f"Process of bot No. {num} is finished")) for num in range(curr, curr + count_of_workers)}
+		
+		return executor, list( _futures.values()), _events
+
+	if platform == "win32":
+		params = []
+		workers = count - _MAX_COUNT_OF_WIN32_WAIT_FOR_MULTIPLE_OBJECTS
+  
+		while workers > 0:
+			params.append(__make(_MAX_COUNT_OF_WIN32_WAIT_FOR_MULTIPLE_OBJECTS))
+			sleep(1)
+			workers -= _MAX_COUNT_OF_WIN32_WAIT_FOR_MULTIPLE_OBJECTS
+		else:
+			params.append(__make(workers + _MAX_COUNT_OF_WIN32_WAIT_FOR_MULTIPLE_OBJECTS))
+			
+	else:
+		executor, _futures, _events = __make(count)
+		params = [(executor, _futures, _events)]
+
 	
-	executor = ProcessPoolExecutor(max_workers=count)
-	_events = [_Event() for _ in range(count)]
-	_futures = {num: executor.submit(__run_bot, bot_class, server, session_id, num, _events[num], **agent_kwds) for num in range(count)}
-	for num, future in (_futures.items()): future.add_done_callback(lambda _: logger.info(f"Process of bot No. {num} is finished"))
 	
-	return _Bots_Manager(executor, list(_futures.values()), _events)
+	return _Bots_Manager(params)
 	
 __all__ = [Agent, Action, get_session_id, spawn_bots, make_env, cleanup]
