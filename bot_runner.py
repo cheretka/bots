7dafc23ce9176dc87b8f27df033a2707b253d8a4
Kamil Chrustowski <chrustek123456789@gmail.com>
2022-02-21 00:31:10 +0100

    Updated example

--- bot_runner.py
+++ bot_runner.py
@@ -1,15 +1,25 @@
 from time import sleep
-from src import make_env, get_session_id, spawn_bots, RandomAgent, CloseFoodAgent, cleanup
+from src import spawn_bots, RandomAgent
 import numpy as np
+import signal
+
+global_done = False
+def terminate_bots(*args, **kwargs):
+	global global_done
+	global_done = True
+
 if __name__ == "__main__":
+	signal.signal(signal.SIGINT, terminate_bots)
 	gen= np.random.default_rng(2137)
 
-# manager = spawn_bots("ws://localhost:2137", get_session_id(), RandomAgent, 1, generator=gen)
-
-	manager = spawn_bots("ws://botbattles.iis.p.lodz.pl:2137", "session_434999395f3cd0f6", RandomAgent, 100, generator=gen)
+	manager = spawn_bots("ws://botbattles.iis.p.lodz.pl:2137", 
+                      	 "session_ca59144551889265", 
+                         RandomAgent, 
+                         1, 
+                         generator=gen)
 
 	try:
-		while True:
+		while not global_done:
 			sleep(0.1)
 	except:...
 	finally: manager.terminate(30)	
